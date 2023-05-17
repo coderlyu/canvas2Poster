68e2f78925c06b674909e9e3279f47da5a1c1e05
coderly <974257574@qq.com>
2023-05-18 00:57:56 +0800

    chore: vuepress

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,9 @@
+const head = require("./head")
+const theme = require('./theme')
+module.exports = {
+  title: 'canvas2posterjs',
+  description: 'canvas2posterjs使用文档',
+  dist: 'web',
+  head,
+  themeConfig: theme
+}
---
+++ docs/.vuepress/theme.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // sidebar: 'auto'
+  sidebar: [
+    {
+      title: "介绍", // 必要的
+      path: "/usage/readme", // 可选的, 标题的跳转链接，应为绝对路径且必须存在
+      collapsable: false, // 可选的, 默认值是 true,
+    }
+  ],
+};
